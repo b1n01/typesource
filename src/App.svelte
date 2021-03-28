ccbf61ca51cbfa9d955ebba159ac0094a7c1c88c
Luca Lorenzini <chielorenz@gmail.com>
2021-03-28 19:40:48 +0200

    Add monaco editor and move components into a dedicated folder

--- src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-</script>
-
-<main>
-	<h1>Typesource</h1>
-</main>
-
-<style>
-</style>
---
+++ src/components/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Monaco from './Monaco.svelte'
+
+</script>
+
+<main>
+	<h1 class="text-yellow-500">Typesource</h1>
+	<Monaco />
+</main>
+
+<style>
+</style>
