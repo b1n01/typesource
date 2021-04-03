77dffa2da759d28bb1879769cca2724859a2e4f7
Luca Lorenzini <chielorenz@gmail.com>
2021-04-03 12:31:50 +0200

    Customize tailwind colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [
     './src/**/*.svelte',
@@ -5,7 +7,17 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans], // Set Nunito as default sans font family
+      },
+      colors: {
+        'background': '#1F2937',
+        'float': '#283548',
+        'highlight': '#3C4A5D',
+        'border': '#49596E',
+      }
+    },
   },
   variants: {
     extend: {},
