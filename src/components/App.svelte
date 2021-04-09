2939523c08fa157acbabb460becba748a46edbf0
Luca Lorenzini <chielorenz@gmail.com>
2021-04-10 01:53:37 +0200

    Remove status bar

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,32 +1,19 @@
 <script>
 	import Nav from './Nav.svelte'
-	import StatusBar from './StatusBar.svelte'
 	import Editor from './Editor.svelte'
 	import Explorer from './Explorer.svelte'
 	// import Match from './Match.svelte'
 </script>
 
 <Nav />
-<section class="container mx-auto">
-	<section class="flex">
-		<section class="w-3/5 m-4">
-			<StatusBar />
-		</section>
-		<section class="w-2/5 m-4"><!-- placeholder --></section>
+<section class="container mx-auto flex">
+	<section class="w-3/5 m-8 mr-4">
+		<Editor />
 	</section>
-	<section class="flex">
-		<section class="w-3/5 ml-8 mr-4">
-			<Editor />
-		</section>
-		<section class="w-2/5 mr-8 ml-4">
-			<section>
-				<Explorer />
-			</section>
-			<!-- 
-			<section class="mt-8">
-				<Match /> 
-			</section>
-			-->
+	<section class="w-2/5 m-8 ml-4">
+		<section>
+			<Explorer />
 		</section>
 	</section>
+</section>
 </section>
--- src/components/StatusBar.svelte
+++ src/components/StatusBar.svelte
@@ -1,25 +1,5 @@
 <script>
-    import { state, sessionStar, selectedFile, availableStates, language } from '../stores.js'
-
-    let timer = '00:00' // default timer
-    let interval = null // id of the current interval
-
-    // Update timer since last session start
-    const updateTimer = () => {
-        let delta = Math.floor((Date.now() - $sessionStar) / 1000)
-        let minutes = Math.floor(delta / 60) % 60;
-        delta -= minutes * 60;
-        let seconds = delta % 60
-        timer = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
-    }
-    
-    // If active immidiately update timer then set interval, otherwise clear interval
-    $: if($state === availableStates.active) {
-        updateTimer() 
-        interval = setInterval(updateTimer, 1000)
-    } else {
-        clearInterval(interval)
-    }
+    import { state, selectedFile, language } from '../stores.js'
 </script>
 
 <section class="text-white text-right text-sm pr-2">
@@ -27,7 +7,6 @@
         <span class="mr-3 pr-3 border-r-2 border-border">file: <span class="font-bold overflow-hidden overflow-ellipsis inline-block align-bottom max-w-xs">{$selectedFile?.name || ''}</span></span>
         <span class="mr-3 pr-3 border-r-2 border-border">language: <span class="font-bold">{$language || ''}</span></span>
         <span class="mr-3 pr-3 border-r-2 border-border">state: <span class="font-bold">{$state}</span></span>
-        <span>time: <span class="font-bold">{timer}</span></span>
     {:else}
         <span>Select a file ↘️</span>
     {/if}
