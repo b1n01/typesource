1a6ab5cbdf5c2b6e122022682ec521822c46c008
Luca Lorenzini <chielorenz@gmail.com>
2021-04-11 13:39:43 +0200

    Update infobox timer to show paused state

--- src/components/InfoBox.svelte
+++ src/components/InfoBox.svelte
@@ -1,8 +1,17 @@
 <script>
-  import { timer, accuracy, wpm } from "../stores";
+  import { timer, accuracy, wpm, elapsed } from "../stores";
+  import { state } from "../states";
+
+  let showTimer = true;
+  setInterval(() => {
+    showTimer = $state.value === "paused" ? !showTimer : true;
+  }, 1000);
 
   $: items = [
-    { label: "time", data: $timer },
+    {
+      label: `time ${$state.value === "paused" ? "⏸️" : ""}`,
+      data: showTimer ? $timer : $timer.replace(":", " "),
+    },
     { label: "wpm", data: $wpm || "-" },
     { label: "accuracy", data: $accuracy ? `${$accuracy}%` : "-" },
   ];
@@ -12,10 +21,10 @@
   {#each items as item, index}
     <div class="flex flex-col text-center">
       <span class="text-opacity-70 text-sm">{item.label}</span>
-      <span class="text-xl">{item.data}</span>
+      <span class="text-xl font-mono">{item.data}</span>
     </div>
     {#if index !== items.length - 1}
-      <span class="py-4 border border-border last:border-0" />
+      <span class="my-2 border border-border" />
     {/if}
   {/each}
 </div>
