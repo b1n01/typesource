f92ae2dec9fd97822c689de8b6c0fb09b0df0191
Luca Lorenzini <chielorenz@gmail.com>
2021-04-19 23:08:17 +0200

    Add disabled style to Button component

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,14 +1,23 @@
 <script>
   export let label = ""; // button text
   export let danger = false; // use a 'danger' style
+  export let disabled = false; // use 'disable' style
+
   let propClass = "";
   export { propClass as class }; // use like `class="class-to-add"`
+
+  let style = "";
+  if (disabled) {
+    style = "bg-gray-700 border-gray-700 text-gray-400 cursor-default";
+  } else if (danger) {
+    style = "bg-red-800 border-red-800 hover:bg-red-700";
+  } else {
+    style = "bg-highlight border-border hover:bg-border";
+  }
 </script>
 
 <button
-  class="border py-2 px-4 rounded focus:outline-none {danger
-    ? 'bg-red-800 border-red-800 hover:bg-red-700'
-    : 'bg-highlight border-border hover:bg-border'} {propClass}"
+  class="border py-2 px-4 rounded focus:outline-none {style} {propClass}"
   on:click
 >
   {label}
