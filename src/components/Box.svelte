0f045f1919654872780c2fe565c3bed5bc03efd6
Luca Lorenzini <chielorenz@gmail.com>
2021-09-11 18:35:31 +0200

    Add 'Box' component

---
+++ src/components/Box.svelte
@@ -0,0 +1,8 @@
+<script>
+  let propClass = "";
+  export { propClass as class }; // use like `class="class-to-add"`
+</script>
+
+<section class="p-4 rounded bg-float text-white {propClass}">
+  <slot />
+</section>
