80600d5485b2e64906a005ade79dc48712d88939
Luca Lorenzini <chielorenz@gmail.com>
2021-09-18 12:48:29 +0200

    Add Stats section (with chart)

--- src/components/Stats.svelte
+++ src/components/Stats.svelte
@@ -1,87 +1,293 @@
 <script>
-  import { collection, query, where, getDocs } from "firebase/firestore";
+  import {
+    collection,
+    query,
+    where,
+    orderBy,
+    getDocs,
+  } from "firebase/firestore";
   import { db } from "../firebase";
   import { user } from "../stores";
   import Loader from "./Loader.svelte";
   import Box from "./Box.svelte";
+  import Chart from "chart.js/auto";
 
   let loading = true; // whether is fetching data from the db
-  let totalSessions = 0;
-  let totalAvgWpm = 0;
-  let totalAvgAccuracy = 0;
+  let isEmpty = false; // wheter there are no data to display
+  let todayStats = []; // stats to be rendered whith today data
+  let totalStats = []; // stats to be rendered with total data
+  let today = new Date().toLocaleDateString("en-US");
 
-  let todaySessions = 0;
-  let todayAvgWpm = 0;
-  let todayAvgAccuracy = 0;
-  let todayData = [];
-  let totalData = [];
+  const chartColors = {
+    sessions: "#e07a5f",
+    wpm: "#f2cc8f",
+    accuracy: "#81b29a",
+    x: "#666",
+    legend: "#999",
+  };
 
+  const chartOptions = {
+    animation: false,
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: chartColors.legend,
+        },
+        grid: {
+          color: chartColors.x,
+          borderColor: chartColors.x,
+        },
+      },
+      // ySessions: {
+      //   title: {
+      //     display: true,
+      //     text: "Number of sessions",
+      //     color: chartColors.sessions,
+      //   },
+      //   ticks: {
+      //     color: chartColors.sessions,
+      //   },
+      //   grid: {
+      //     borderColor: chartColors.sessions,
+      //     drawOnChartArea: false,
+      //   },
+      // },
+      yWpm: {
+        title: {
+          display: true,
+          text: "Words per minute",
+          color: chartColors.wpm,
+        },
+        position: "left",
+        ticks: {
+          color: chartColors.wpm,
+        },
+        grid: {
+          borderColor: chartColors.wpm,
+          drawOnChartArea: false,
+        },
+      },
+      yAccuracy: {
+        title: {
+          display: true,
+          text: "Accuracy",
+          color: chartColors.accuracy,
+        },
+        position: "right",
+        ticks: {
+          color: chartColors.accuracy,
+        },
+        grid: {
+          borderColor: chartColors.accuracy,
+          drawOnChartArea: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: {
+          color: "#FFF",
+          padding: 32,
+          font: {
+            size: 14,
+          },
+        },
+      },
+      tooltip: {
+        titleAlign: "center",
+        bodySpacing: 8,
+        padding: 12,
+        titleFont: {
+          size: 14,
+        },
+        bodyFont: {
+          size: 14,
+        },
+        callbacks: {
+          label: (context) => {
+            // Format each tooltip label
+            let label = context.dataset.label;
+            label = " " + label; // add left space
+            return label + ": " + context.parsed.y;
+          },
+          title: (context) => {
+            // Format tooltip title date
+            let date = new Date(context[0].label);
+            let options = {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            };
+            return date.toLocaleDateString("en-US", options);
+          },
+        },
+      },
+    },
+  };
+
+  // Get empty stats
+  function initStats() {
+    return {
+      sessions: 0,
+      wpm: 0,
+      acc: 0,
+      today: {
+        sessions: 0,
+        wpm: 0,
+        acc: 0,
+      },
+    };
+  }
+
+  // Create the chart
+  function initChart(canvas, series, labels) {
+    return new Chart(canvas, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+          // {
+          //   label: "sessions",
+          //   data: sessionsDataset,
+          //   backgroundColor: chartColors.sessions,
+          //   borderColor: chartColors.sessions,
+          //   tension: 0.3,
+          //   yAxisID: "ySessions",
+          // },
+          {
+            label: "wpm",
+            data: series.wpm,
+            backgroundColor: chartColors.wpm,
+            borderColor: chartColors.wpm,
+            tension: 0.3,
+            yAxisID: "yWpm",
+          },
+          {
+            label: "accuracy",
+            data: series.acc,
+            backgroundColor: chartColors.accuracy,
+            borderColor: chartColors.accuracy,
+            tension: 0.3,
+            yAxisID: "yAccuracy",
+          },
+        ],
+      },
+      options: chartOptions,
+    });
+  }
+
+  // When the user is ready fetch data from DB
   user.subscribe(async () => {
     if ($user) {
       const metricsColl = collection(db, "metrics");
-      const select = query(metricsColl, where("uid", "==", $user.uid));
-      const results = await getDocs(select);
+      const select = query(
+        metricsColl,
+        where("uid", "==", $user.uid),
+        orderBy("timestamp")
+      );
+      const metrics = await getDocs(select);
 
-      let today = new Date();
-      let totalWpm = 0;
-      let totalAccuracy = 0;
-      let todayWpm = 0;
-      let todayAccuracy = 0;
-
-      results.forEach((metricSnap) => {
-        let metric = metricSnap.data();
-
-        totalSessions++;
-        totalWpm += metric.wpm;
-        totalAccuracy += metric.accuracy;
-
-        let metricDate = new Date(metric.timestamp.toDate());
-        if (
-          metricDate.toLocaleDateString("en-US") ==
-          today.toLocaleDateString("en-US")
-        ) {
-          todaySessions++;
-          todayWpm += metric.wpm;
-          todayAccuracy += metric.accuracy;
-        }
-      });
-
-      if (totalSessions) {
-        totalAvgWpm = Math.round(totalWpm / totalSessions);
-        totalAvgAccuracy = Math.round(totalAccuracy / totalSessions);
-      }
-
-      if (todaySessions) {
-        todayAvgWpm = Math.round(todayWpm / todaySessions);
-        todayAvgAccuracy = Math.round(todayAccuracy / todaySessions);
-      }
-
-      todayData = [
-        { label: "Today sessions", content: todaySessions },
-        { label: "Today wpm", content: todayAvgWpm },
-        { label: "Today accuracy", content: todayAvgAccuracy + "%" },
-      ];
-
-      totalData = [
-        { label: "Total sessions", content: totalSessions },
-        { label: "Total wpm", content: totalAvgWpm },
-        { label: "Total accuracy", content: totalAvgAccuracy + "%" },
-      ];
+      calculateStats(metrics);
+      drawChart(metrics);
 
       loading = false;
     }
   });
+
+  // Calculates statistics from user metrics and puts them into todayStats and totalStats
+  const calculateStats = (metrics) => {
+    let stats = initStats();
+    let totals = initStats();
+
+    metrics.forEach((metrics) => {
+      let metric = metrics.data();
+      let time = metric.timestamp.toDate();
+      let metricDate = new Date(time).toLocaleDateString("en-US");
+
+      totals.sessions++;
+      totals.wpm += metric.wpm;
+      totals.acc += metric.accuracy;
+
+      if (metricDate == today) {
+        totals.today.sessions++;
+        totals.today.wpm += metric.wpm;
+        totals.today.acc += metric.accuracy;
+      }
+    });
+
+    if (totals.sessions) {
+      stats.sessions = totals.sessions;
+      stats.wpm = Math.round(totals.wpm / totals.sessions);
+    }
+
+    if (totals.today.sessions) {
+      stats.today.sessions = totals.today.sessions;
+      stats.today.wpm = Math.round(totals.today.wpm / totals.today.sessions);
+    }
+
+    totalStats = [
+      { label: "Total sessions", content: stats.sessions },
+      { label: "Total wpm", content: stats.wpm },
+      { label: "Total accuracy", content: stats.acc + "%" },
+    ];
+
+    todayStats = [
+      { label: "Today sessions", content: stats.today.sessions },
+      { label: "Today wpm", content: stats.today.wpm },
+      { label: "Today accuracy", content: stats.today.acc + "%" },
+    ];
+  };
+
+  // Render the chart
+  const drawChart = (metrics) => {
+    isEmpty = metrics.size == 0;
+    let series = { sessions: [], wpm: [], acc: [] };
+    let labels = [];
+    let dates = {};
+
+    // Group metrics by bate
+    metrics.forEach((metricData) => {
+      let metric = metricData.data();
+      let time = metric.timestamp.toDate();
+      let date = new Date(time).toLocaleDateString("en-US");
+
+      let stats = { accuracy: metric.accuracy, wpm: metric.wpm };
+      dates[date] ? dates[date].push(stats) : (dates[date] = [stats]);
+    });
+
+    // Add data to the datasets for the chart
+    for (let day in dates) {
+      let stats = dates[day];
+      let sessions = stats.length;
+      let wpm = stats.reduce((sum, stat) => sum + stat.wpm, 0);
+      let accuracy = stats.reduce((sum, stat) => sum + stat.accuracy, 0);
+
+      labels.push(day);
+      series.sessions.push(sessions);
+      series.wpm.push(sessions ? Math.round(wpm / sessions) : 0);
+      series.acc.push(sessions ? Math.round(accuracy / sessions) : 0);
+    }
+
+    const canvas = document.getElementById("chart").getContext("2d");
+    initChart(canvas, series, labels);
+  };
 </script>
 
 <div class="p-8 w-full">
-  {#if loading}
-    <span class="h-32 flex justify-center items-center">
-      <Loader />
-    </span>
-  {:else}
+  <span
+    class="h-32 flex justify-center items-center {!loading ? 'hidden' : ''}"
+  >
+    <Loader />
+  </span>
+  <div class={loading ? "hidden" : ""}>
     <div class="flex w-full">
       <Box class="mr-2 w-1/2 flex items-center justify-between">
-        {#each todayData as data}
+        {#each todayStats as data}
           <div class="px-10 border-l-2 first:border-0 border-border">
             <p>{data.label}</p>
             <p class="mt-4 text-3xl font-bold">{data.content}</p>
@@ -89,7 +295,7 @@
         {/each}
       </Box>
       <Box class="ml-2 w-1/2 flex items-center justify-between">
-        {#each totalData as data}
+        {#each totalStats as data}
           <div class="px-10 border-l-2 first:border-0 border-border">
             <p>{data.label}</p>
             <p class="mt-4 text-3xl font-bold">{data.content}</p>
@@ -97,6 +303,14 @@
         {/each}
       </Box>
     </div>
-    <Box class="my-4 w-full">Trend Chart</Box>
-  {/if}
+    <Box class="my-5 px-32 pt-8">
+      {#if isEmpty}
+        <p>
+          No data to show 😳. Play some sessions to start gather usefull data!
+        </p>
+      {:else}
+        <canvas id="chart" />
+      {/if}
+    </Box>
+  </div>
 </div>
