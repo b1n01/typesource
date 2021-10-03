7ddbaee78783a63d2178475753b902284b3fe334
Luca Lorenzini <chielorenz@gmail.com>
2021-10-03 17:36:20 +0200

    Clean up Infobox

--- src/components/InfoBox.svelte
+++ src/components/InfoBox.svelte
@@ -1,29 +1,6 @@
 <script>
-  import { timer, metrics, elapsed } from "../stores";
+  import { timer, metrics } from "../stores";
   import { userState } from "../states";
-
-  let showTimer = true;
-  setInterval(() => {
-    showTimer = $userState.matches("offline.paused") ? !showTimer : true;
-  }, 1000);
-
-  // let displayWpm = "-";
-  // let displayAccuracy = "-";
-  // let canUpdate = false;
-
-  // We wand to update the infobox once per second, so we use canUpdate as
-  // a traffic light: when we update the info we than set it to false, but
-  // every time $elapsed changes (every second) we set canUpdate to true
-  // $: {
-  // Just set true each time $elapse changes (each second)
-  // canUpdate = $elapsed || true;
-  // }
-
-  // $: if (canUpdate) {
-  // canUpdate = false;
-  // displayWpm = $metrics.wpm || "-";
-  // displayAccuracy = $metrics.accuracy ? `${$metrics.accuracy}%` : "-";
-  // }
 </script>
 
 <div class="text-white mx-4 flex justify-between">
@@ -43,8 +20,6 @@
     <span class="text-opacity-70 text-sm">
       time {$userState.matches("offline.paused") ? "⏸️" : ""}
     </span>
-    <span class="text-xl font-mono">
-      {showTimer ? $timer : $timer.replace(":", " ")}
-    </span>
+    <span class="text-xl font-mono">{$timer}</span>
   </div>
 </div>
