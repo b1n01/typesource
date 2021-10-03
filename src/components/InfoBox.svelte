814b3eeeda46423ec5fa39b2a197f14e7fe510f9
Luca Lorenzini <chielorenz@gmail.com>
2021-10-03 17:39:19 +0200

    Fix Infobox data (they were swapped)

--- src/components/InfoBox.svelte
+++ src/components/InfoBox.svelte
@@ -6,14 +6,14 @@
 <div class="text-white mx-4 flex justify-between">
   <div class="flex flex-col text-center">
     <span class="text-opacity-70 text-sm">accuracy</span>
-    <span class="text-xl font-mono">{$metrics.wpm || "-"}</span>
+    <span class="text-xl font-mono"
+      >{$metrics.accuracy ? `${$metrics.accuracy}%` : "-"}
+    </span>
   </div>
   <span class="my-2 border border-border" />
   <div class="flex flex-col text-center">
     <span class="text-opacity-70 text-sm">wpm</span>
-    <span class="text-xl font-mono">
-      {$metrics.accuracy ? `${$metrics.accuracy}%` : "-"}
-    </span>
+    <span class="text-xl font-mono">{$metrics.wpm || "-"} </span>
   </div>
   <span class="my-2 border border-border" />
   <div class="flex flex-col text-center">
