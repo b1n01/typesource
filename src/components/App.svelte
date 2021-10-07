ee45ab1d81f5122074483ca360d1f52831c992f4
Luca Lorenzini <chielorenz@gmail.com>
2021-10-07 13:28:12 +0200

    Move Chart into the new Modal component

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -8,6 +8,7 @@
   import InfoBox from "./InfoBox.svelte";
   import Stats from "./Stats.svelte";
   import Chart from "./Chart.svelte";
+  import Modal from "./Modal.svelte";
   import { userState } from "../states";
 </script>
 
@@ -15,24 +16,23 @@
   <Nav />
   <div class="mx-auto flex min-w-lg max-w-screen-2xl">
     <Route path="/">
-      {#if $userState.matches("offline.ended")}
+      <div class="w-1/3 m-4 mr-2 xl:m-8 xl:mr-4 2xl:w-1/4">
+        <div class="mb-4 xl:mb-8">
+          <InfoBox />
+        </div>
+        <div class="mb-4 xl:mb-8">
+          <Explorer />
+        </div>
+        <div>
+          <Match />
+        </div>
+      </div>
+      <div class="w-2/3 m-4 ml-2 xl:m-8 xl:ml-4 2xl:w-3/4">
+        <Editor />
+      </div>
+      <Modal visible={$userState.matches("offline.ended")}>
         <Chart />
-      {:else}
-        <div class="w-1/3 m-4 mr-2 xl:m-8 xl:mr-4 2xl:w-1/4">
-          <div class="mb-4 xl:mb-8">
-            <InfoBox />
-          </div>
-          <div class="mb-4 xl:mb-8">
-            <Explorer />
-          </div>
-          <div>
-            <Match />
-          </div>
-        </div>
-        <div class="w-2/3 m-4 ml-2 xl:m-8 xl:ml-4 2xl:w-3/4">
-          <Editor />
-        </div>
-      {/if}
+      </Modal>
     </Route>
     <Route path="stats">
       <Stats />
---
+++ src/components/Modal.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let visible = false;
+</script>
+
+{#if visible}
+  <div class="fixed inset-0 bg-gray-900 bg-opacity-70 z-10">
+    <div class="mx-auto pt-12 relative flex min-w-lg max-w-screen-2xl">
+      <slot />
+    </div>
+  </div>
+{/if}
