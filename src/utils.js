64aafa820a4adbb2f57d510f29a5059ca5259127
Luca Lorenzini <chielorenz@gmail.com>
2021-11-02 20:17:04 +0100

    Add a custom mapping for non supported file extensions by Monaco

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,21 @@
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 
+// Define custom mapping for non supported file extensions
+const customLanguages = {
+  svelte: "javascript",
+  vue: "javascript",
+};
+
 // get languages id from an url with a file name on its path
 export const getLanguageFromUrl = (url) => {
   const extension = url.split(".").pop();
-  const languages = monaco.languages.getLanguages();
-  const language = languages.find((lang) =>
-    lang.extensions.includes("." + extension)
-  );
-  return language ? language.id : "plaintext";
+  if (customLanguages[extension]) {
+    return customLanguages[extension];
+  } else {
+    const languages = monaco.languages.getLanguages();
+    const language = languages.find((lang) =>
+      lang.extensions.includes("." + extension)
+    );
+    return language ? language.id : "plaintext";
+  }
 };
